5bb39ff10703b7907b5e39d4b019e0cc6d507acc
julgrahn <alexander.grahn@hotmail.se>
2020-07-07 00:12:22 +0200

    find coordinates for upper left corner of face

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 img = cv2.imread("facephoto.jpg")
 grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+faces = face_cascade.detectMultiScale(grayImage,
+scaleFactor = 1.05,
+minNeighbors = 5)
+
+print(type(faces))
+print(faces)
+
 cv2.imshow("Gray", grayImage)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
